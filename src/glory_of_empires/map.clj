70145e9c5f46f4a15f21a4428e0c1074f7c1ff0b
Robert Brotherus <robert.brotherus@napa.fi>
2015-11-21 23:46:14 +0200

    move systems data to systems.clj

--- src/glory_of_empires/map.clj
+++ src/glory_of_empires/map.clj
@@ -4,102 +4,12 @@
   (:use clojure-common.xml)
   (:use clojure.test)
   (:use ring.adapter.jetty)
+  (:use glory-of-empires.systems)
   (:gen-class))
 
 (def resources-url "http://www.brotherus.net/ti3/")
 
-(def all-systems
-  [ { :image "1planet/Tile-Acheron.gif" }
-    { :image "1planet/Tile-Aeon.gif" }
-    { :image "1planet/Tile-Aker.gif" }
-    { :image "1planet/Tile-Ammit.gif" }
-    { :image "1planet/Tile-Amun.gif" }
-    { :image "1planet/Tile-Andjety.gif" }
-    { :image "1planet/Tile-Anhur.gif" }
-    { :image "1planet/Tile-Ankh.gif" }
-    { :image "1planet/Tile-Anuket.gif" }
-    { :image "1planet/Tile-Apis.gif" }
-    { :image "1planet/Tile-Asgard.gif" }
-    { :image "1planet/Tile-Asgard_III.gif" }
-    { :image "1planet/Tile-Astennu.gif" }
-    { :image "1planet/Tile-Aten.gif" }
-    { :image "1planet/Tile-Babi.gif" }
-    { :image "1planet/Tile-Bakha.gif" }
-    { :image "1planet/Tile-Bast.gif" }
-    { :image "1planet/Tile-Beriyil.gif" }
-    { :image "1planet/Tile-Bes.gif" }
-    { :image "1planet/Tile-Capha.gif" }
-    { :image "1planet/Tile-Chensit.gif" }
-    { :image "1planet/Tile-Chnum.gif" }
-    { :image "1planet/Tile-Chuuka.gif" }
-    { :image "1planet/Tile-Cicerus.gif" }
-    { :image "1planet/Tile-Coruscant.gif" }
-    { :image "1planet/Tile-Dedun.gif" }
-    { :image "1planet/Tile-Deimo.gif" }
-    { :image "1planet/Tile-Discworld.gif" }
-    { :image "1planet/Tile-Dune.gif" }
-    { :image "1planet/Tile-Elnath.gif" }
-    { :image "1planet/Tile-Everra.gif" }
-    { :image "1planet/Tile-Faunus.gif" }
-    { :image "1planet/Tile-Fiorina.gif" }
-    { :image "1planet/Tile-FloydIV.gif" }
-    { :image "1planet/Tile-Garbozia.gif" }
-    { :image "1planet/Tile-Heimat.gif" }
-    { :image "1planet/Tile-Hopes_End.gif" }
-    { :image "1planet/Tile-Inaak.gif" }
-    { :image "1planet/Tile-Industrex.gif" }
-    { :image "1planet/Tile-Iskra.gif" }
-    { :image "1planet/Tile-Ithaki.gif" }
-    { :image "1planet/Tile-Kanite.gif" }
-    { :image "1planet/Tile-Kauket.gif" }
-    { :image "1planet/Tile-Kazenoeki.gif" }
-    { :image "1planet/Tile-Khepri.gif" }
-    { :image "1planet/Tile-Khnum.gif" }
-    { :image "1planet/Tile-Klendathu.gif" }
-    { :image "1planet/Tile-Kobol.gif" }
-    { :image "1planet/Tile-Laurin.gif" }
-    { :image "1planet/Tile-Lesab.gif" }
-    { :image "1planet/Tile-Lodor.gif" }
-    { :image "1planet/Tile-Lv426.gif" }
-    { :image "1planet/Tile-Medusa_V.gif" }
-    { :image "1planet/Tile-Mehar_Xull.gif" }
-    { :image "1planet/Tile-Mirage.gif" }
-    { :image "1planet/Tile-Myrkr.gif" }
-    { :image "1planet/Tile-Nanan.gif" }
-    { :image "1planet/Tile-Natthar.gif" }
-    { :image "1planet/Tile-Nef.gif" }
-    { :image "1planet/Tile-Nexus.gif" }
-    { :image "1planet/Tile-Niiwa-Sei.gif" }
-    { :image "1planet/Tile-Pakhet.gif" }
-    { :image "1planet/Tile-Parzifal.gif" }
-    { :image "1planet/Tile-Perimeter.gif" }
-    { :image "1planet/Tile-Petbe.gif" }
-    { :image "1planet/Tile-Primor.gif" }
-    { :image "1planet/Tile-Ptah.gif" }
-    { :image "1planet/Tile-Qetesh.gif" }
-    { :image "1planet/Tile-Quann.gif" }
-    { :image "1planet/Tile-Radon.gif" }
-    { :image "1planet/Tile-Saudor.gif" }
-    { :image "1planet/Tile-Sem-Lore.gif" }
-    { :image "1planet/Tile-Shai.gif" }
-    { :image "1planet/Tile-Shool.gif" }
-    { :image "1planet/Tile-Solitude.gif" }
-    { :image "1planet/Tile-Sulako.gif" }
-    { :image "1planet/Tile-Suuth.gif" }
-    { :image "1planet/Tile-Tarmann.gif" }
-    { :image "1planet/Tile-Tefnut.gif" }
-    { :image "1planet/Tile-Tempesta.gif" }
-    { :image "1planet/Tile-Tenenit.gif" }
-    { :image "1planet/Tile-Theom.gif" }
-    { :image "1planet/Tile-Thibah.gif" }
-    { :image "1planet/Tile-Ubuntu.gif" }
-    { :image "1planet/Tile-Vefut_II.gif" }
-    { :image "1planet/Tile-Wadjet.gif" }
-    { :image "1planet/Tile-Wellon.gif" }
-    { :image "1planet/Tile-Wepwawet.gif" } ] )
-
-
-; ---- coordinate tools
+; ----------- coordinate tools --------------------
 
 (defn min-pos
   { :test (fn [] (is (= { :x 7 :y -4 } (min-pos [ { :x 7 :y 12} { :x 8 :y -4 } ] )))) }
