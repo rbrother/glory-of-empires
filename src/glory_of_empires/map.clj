9d00447c5dc00beb16963af9fd1a7a7157baefb5
Robert Brotherus <robert@iki.fi>
2015-11-22 20:38:23 +0200

    Move to use simple [ x y] position vectors instead of { :x x :y y }
    Allows usage of powerful vector operations from clojure library
    
    More tests for map-making

--- src/glory_of_empires/map.clj
+++ src/glory_of_empires/map.clj
@@ -12,35 +12,38 @@
 ; ----------- coordinate tools --------------------
 
 (defn min-pos
-  { :test (fn [] (is (= { :x 7 :y -4 } (min-pos [ { :x 7 :y 12} { :x 8 :y -4 } ] )))) }
-  [ coords ] { :x (apply min (map :x coords)) :y (apply min (map :y coords)) } )
+  { :test (fn [] (is (= [ 7 -4 ] (min-pos [ [ 7 12 ] [ 8 -4 ] ] )))) }
+  [ vectors ] (apply mapv min vectors))
 
 (defn max-pos
-  { :test (fn [] (is (= { :x 8 :y 12 } (min-pos [ { :x 7 :y 12} { :x 8 :y -4 } ] )))) }
-  [ coords ] { :x (apply max (map :x coords)) :y (apply max (map :y coords)) } )
+  { :test (fn [] (is (= [ 8 12 ] (min-pos [ [ 7 12 ] [ 8 -4 ] ] )))) }
+  [ vectors ] (apply mapv max vectors))
 
-(defn distance [dx dy] (Math/sqrt (+ (* dx dx) (* dy dy))))
+(defn distance [ vec1 ] (Math/sqrt (apply + (map * vec1 vec1))))
 
-(defn add-pos [ { x1 :x y1 :y } { x2 :x y2 :y } ] { :x (+ x1 x2) :y (+ y1 y2) } )
-(defn sub-pos [ { x1 :x y1 :y } { x2 :x y2 :y } ] { :x (- x1 x2) :y (- y1 y2) } )
-(defn mul-pos [ { x :x y :y } m ] { :x (* x m) :y (* y m) } )
+(defn add-pos [ vec1 vec2 ] (map + vec1 vec2) )
+(defn sub-pos [ vec1 vec2 ] (map - vec1 vec2) )
+(defn mul-pos [ vec1 scalar ] (map * vec1 (repeat scalar)))
 
 ; -------------------------- map ---------------------------------
 
-(def tile-size { :x 432 :y 376 } )
+(def tile-size [ 432 376 ] )
 
-(defn- screen-pos
-  { :test (fn [] (is (= { :x 0.0 :y 0.0 } (screen-pos { :x 0 :y 0 } )))) }
-  [ { logical-x :x logical-y :y } ]
-    { :x (* logical-x (tile-size :x) 0.75)
-      :y (* (tile-size :y) (+ (* logical-x 0.5) logical-y)) } )
+(defn- screen-loc
+  { :test (fn [] (is (= { :x 0.0 :y 0.0 } (screen-loc { :x 0 :y 0 } )))) }
+  [ [ logical-x logical-y ] ]
+    [ (* logical-x (first tile-size) 0.75)
+      (* (last tile-size) (+ (* logical-x 0.5) logical-y)) ] )
+
+(defn screen-locs [ map-pieces ]
+  (->> map-pieces (map :logical-pos) (map screen-loc)))
 
 (defn- tile-on-table? [ piece map-size ]
-  (let [ { x :x y :y } (screen-pos (:logical-pos piece)) ]
-    (< (distance x y) (* (tile-size :y) map-size 1.01 ))))
+  (let [ pos (screen-loc (:logical-pos piece)) ]
+    (< (distance pos) (* (last tile-size) map-size 1.01 ))))
 
 (defn- random-system [ x y ]
-  { :logical-pos { :x x :y y } :system (rand-nth all-systems) } )
+  { :logical-pos [ x y ] :system (rand-nth all-systems) } )
 
 (defn make-random-map [ rings ]
   (let [ a-range (range (- rings) (inc rings)) ]
@@ -48,23 +51,22 @@
          (map (fn [ [x y] ] (random-system x y) ))
          (filter #(tile-on-table? % rings)) )))
 
-
 ;------------------ to svg ------------------------
 
 (defn- piece-to-svg [ { logical-pos :logical-pos system :system :as tile } ]
-  (let [ { x :x y :y } (screen-pos logical-pos) ]
-    [ :image { :x (int x) :y (int y) :width (tile-size :x) :height (tile-size :y)
+  (let [ [ x y ] (screen-loc logical-pos) ]
+    [ :image { :x (int x) :y (int y) :width (first tile-size) :height (last tile-size)
                "xlink:href" (str resources-url "Tiles/" (system :image)) } ] ))
 
 (defn map-to-svg [ map-pieces scale ]
-  (let [ screen-locs (->> map-pieces (map :logical-pos) (map screen-pos))
-         min-p (min-pos screen-locs)
-         max-p (max-pos screen-locs)
-         size (mul-pos (add-pos (sub-pos max-p min-p) tile-size) scale)
-         counter-translate (str (- (min-p :x)) "," (- (min-p :y))) ]
+  (let [ s-locs (screen-locs map-pieces)
+         min-p (min-pos s-locs)
+         max-p (max-pos s-locs)
+         size (mul-pos (map + (map - max-p min-p) tile-size) scale)
+         counter-translate (str (- (first min-p)) "," (- (last min-p))) ]
     [ :html {}
       [ :body { :style "background: #202020;" }
-        [ :svg { :width (:x size), :height (:y size), "xmlns:xlink" "http://www.w3.org/1999/xlink" }
+        [ :svg { :width (first size), :height (last size), "xmlns:xlink" "http://www.w3.org/1999/xlink" }
           (concat [ :g { :transform (str "scale(" scale ") translate(" counter-translate ")") } ]
                   (map piece-to-svg map-pieces)) ] ] ] ))
 
--- test/glory_of_empires/map_test.clj
+++ test/glory_of_empires/map_test.clj
@@ -6,12 +6,32 @@
 
 (deftest random-map-test
   (testing "make-random-map"
-    (is (=
-      (->> (make-random-map 1) (map #(assoc-in % [:system :image] "xxx")))
-      [ { :logical-pos { :x -1 :y 0 } :system { :image "xxx" } }
-        { :logical-pos { :x -1 :y 1 } :system { :image "xxx" } }
-        { :logical-pos { :x 0 :y -1 } :system { :image "xxx" } }
-        { :logical-pos { :x 0 :y 0 } :system { :image "xxx" } }
-        { :logical-pos { :x 0 :y 1 } :system { :image "xxx" } }
-        { :logical-pos { :x 1 :y -1 } :system { :image "xxx" } }
-        { :logical-pos { :x 1 :y 0 } :system { :image "xxx" } } ] ))))
+    (let [ a-map (make-random-map 1)
+           normalized-map (map #(assoc-in % [:system :image] "xxx") a-map)
+           correct-screen-locs [ [-324.0 -188.0] [-324.0 188.0] [0.0 -376.0] [0.0 0.0] [0.0 376.0] [324.0 -188.0] [324.0 188.0] ] ]
+      (is (=
+        normalized-map
+        [ { :logical-pos [ -1 0 ] :system { :image "xxx" } }
+          { :logical-pos [ -1 1 ] :system { :image "xxx" } }
+          { :logical-pos [ 0 -1 ] :system { :image "xxx" } }
+          { :logical-pos [ 0 0 ] :system { :image "xxx" } }
+          { :logical-pos [ 0 1 ] :system { :image "xxx" } }
+          { :logical-pos [ 1 -1 ] :system { :image "xxx" } }
+          { :logical-pos [ 1 0 ] :system { :image "xxx" } } ] ))
+      (is (= (screen-locs a-map) correct-screen-locs ))
+      (is (= (min-pos correct-screen-locs) [-324.0 -376.0] ))
+      (is (= (max-pos correct-screen-locs) [324.0 376.0] ))
+      (is (= (mul-pos (map + (map - [324.0 376.0] [-324.0 -376.0]) tile-size) 0.5) [ 540.0 564.0] ))
+      (is (= (map-to-svg normalized-map 0.5)
+             [:html {}
+              [:body {:style "background: #202020;"}
+                [:svg {:width 540.0, :height 564.0, "xmlns:xlink" "http://www.w3.org/1999/xlink"}
+                  [:g {:transform "scale(0.5) translate(324.0,376.0)"}
+                    [:image {:x -324, :y -188, :width 432, :height 376, "xlink:href" "http://www.brotherus.net/ti3/Tiles/xxx"}]
+                    [:image {:x -324, :y 188, :width 432, :height 376, "xlink:href" "http://www.brotherus.net/ti3/Tiles/xxx"}]
+                    [:image {:x 0, :y -376, :width 432, :height 376, "xlink:href" "http://www.brotherus.net/ti3/Tiles/xxx"}]
+                    [:image {:x 0, :y 0, :width 432, :height 376, "xlink:href" "http://www.brotherus.net/ti3/Tiles/xxx"}]
+                    [:image {:x 0, :y 376, :width 432, :height 376, "xlink:href" "http://www.brotherus.net/ti3/Tiles/xxx"}]
+                    [:image {:x 324, :y -188, :width 432, :height 376, "xlink:href" "http://www.brotherus.net/ti3/Tiles/xxx"}]
+                    [:image {:x 324, :y 188, :width 432, :height 376, "xlink:href" "http://www.brotherus.net/ti3/Tiles/xxx"}]]]]]))
+      )))
