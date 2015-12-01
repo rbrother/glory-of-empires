44cec5476174f24921880e38d369988712b3367d
Robert Brotherus <robert.brotherus@napa.fi>
2015-12-01 07:47:59 +0200

    use xml-to-text at reply, not needed elsewhere

--- src/glory_of_empires/main.clj
+++ src/glory_of_empires/main.clj
@@ -15,14 +15,13 @@
 
 (defn run-command [ command ]
   (swap! game-state command)
-  "done")
+  "ok")
 
 (defn get-map
   ( [] (get-map {}) )
   ( [ opts ]
     (let [ m (@game-state :map) ]
-      (if (nil? m) "No map"
-        (xml-to-text (map-to-svg m opts))))))
+      (if (nil? m) "No map" (map-to-svg m opts)))))
 
 (defn set-random-map
   ( [] (set-random-map { :size 3 } ))
@@ -35,17 +34,18 @@
 (defn reply [ content ]
   { :status 200
     :headers {"Content-Type" "text/html" "Access-Control-Allow-Origin" "*"}
-    :body content }  )
+    :body (xml-to-text content) }  )
+
+(defn handle-exception [ ex ]
+  (println (str ex))
+  [ :span { :style "color: #ff0000;" } (str "Error: " (.getMessage ex)) ] )
 
 (defn handler [request]
   (println "------------ request received -----------")
   (let [ message-text (slurp (:body request)) ]
     (println message-text)
     (let [ message (read-string message-text) ]
-      (reply
-        (try
-          (eval message)
-          (catch Exception e (do (println (str e)) (str "Error: " (.getMessage e)))))))))
+      (reply (try (eval message) (catch Exception e (handle-exception e)))))))
 
 (defn -main [& args]
   (reset! game-state (load-from-file "game-state.clj"))
