bd9b9f63039f313300071d2ce9a052a31d0f5ab4
Robert Brotherus <robert.brotherus@napa.fi>
2015-12-05 17:29:40 +0200

    refactor: make-board-command

--- src/glory_of_empires/command.clj
+++ src/glory_of_empires/command.clj
@@ -2,24 +2,29 @@
   (:require [glory-of-empires.map :as board])
   (:require [glory-of-empires.game-state :as game-state]))
 
-(defn run-command [ command ]
+;------------ command helpers ---------------
+
+(defn- run-command [ command ]
   (swap! game-state/game command)
   "ok")
 
-(defn board-command [ command ]
+(defn- board-command [ command ]
   (run-command (fn [ game ] (update game :map command))))
+
+(defn- make-board-command [ new-board ]
+  (run-command (fn [ state ] (assoc state :map new-board))))
+
+;----------- commands --------------------
 
 (defn round-board
   ( [] (round-board 3) )
   ( [ size ]
-    (run-command
-      (fn [ state ] (assoc state :map (board/round-board size))))))
+    (make-board-command (board/round-board size))))
 
 (defn rect-board
   ( [] (rect-board 3 3) )
   ( [ width height ]
-    (run-command
-      (fn [ state ] (assoc state :map (board/rect-board width height))))))
+    (make-board-command (board/rect-board width height))))
 
 (defn set-systems-random [] (board-command board/random-systems))
 
