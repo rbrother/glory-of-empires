98b3f7c07389a1eea2e9e35a2638a1ea9c57c90b
Robert Brotherus <robert.brotherus@napa.fi>
2015-12-29 20:22:01 +0200

    map_test fixed

--- test/glory_of_empires/map_test.clj
+++ test/glory_of_empires/map_test.clj
@@ -52,7 +52,52 @@
       (group-ships [ :a :b ] [ 1 2 ] )        [ [ [:a ] 1 ] [ [:b] 2 ] ]
       (group-ships [ :a :a ] [ 1 2 ] )        [ [ [:a ] 1 ] [ [:a] 2 ] ]
       (group-ships [ :a :b :c ] [ 1 2 ] )     [ [ [:a :b] 1 ] [ [:c] 2 ] ]
-      (map-to-svg (mini-game-state :map)) []
+      (map-to-svg (mini-game-state :map))
+        [ :svg { :height 282.0 :width 378.0 :xmlns:xlink "http://www.w3.org/1999/xlink" }
+            [ :g { :transform "scale(0.5) translate(0,0)" }
+                [ :g { :id "abyz-fria-system" :transform " translate(0,0)" }
+                    [ :image { :height 376 :width 432 :x 0 :xlink:href "http://localhost/ti3/Tiles/2planet/Tile-Abyz-Fria.gif" :y 0 } ]
+                    [ :g { :id "abyz-fria-units" :transform " translate(216,188)" }
+                        [ :g { :id "abyz-ground-units" :transform " translate(-75,-70)" }
+                            [ :g { :transform " translate(-24,-58)" }
+                                [ :image { :height 57 :width 48 :x 0 :xlink:href "http://localhost/ti3/Ships/Yellow/Unit-Yellow-GF.png" :y 0 } ] ]
+                            [ :g { :transform " translate(-33,6)" }
+                                [ :image { :height 49 :width 67 :x 0 :xlink:href "http://localhost/ti3/Ships/Yellow/Unit-Yellow-PDS.png" :y 0 } ]
+                                [ :g { :id "shaded-text" :transform " translate(67,40)" }
+                                    [ :text { :fill "black" :font-family "Arial" :font-size "45px" :x 2 :y 2 } "2" ]
+                                    [ :text { :fill "white" :font-family "Arial" :font-size "45px" :x 0 :y 0 } "2" ] ] ] ]
+                        [ :g { :id "fria-ground-units" :transform " translate(75,70)" }
+                            [ :g { :transform " translate(-24,-58)" }
+                                [ :image { :height 57 :width 48 :x 0 :xlink:href "http://localhost/ti3/Ships/Yellow/Unit-Yellow-GF.png" :y 0 } ]
+                                [ :g { :id "shaded-text" :transform " translate(48,40)" }
+                                    [ :text { :fill "black" :font-family "Arial" :font-size "45px" :x 2 :y 2 } "2" ]
+                                    [ :text { :fill "white" :font-family "Arial" :font-size "45px" :x 0 :y 0 } "2" ] ] ]
+                            [ :g { :transform " translate(-38,-9)" }
+                                [ :image { :height 78 :width 76 :x 0 :xlink:href "http://localhost/ti3/Ships/Yellow/Unit-Yellow-Spacedock.png" :y 0 } ] ] ]
+                        [ :g { :transform " translate(-115,11)" }
+                            [ :image { :height 139 :width 50 :x 0 :xlink:href "http://localhost/ti3/Ships/Yellow/Unit-Yellow-Carrier.png" :y 0 } ]
+                            [ :g { :id "shaded-text" :transform " translate(0,104)" }
+                                [ :text { :fill "black" :font-family "Arial" :font-size "22px" :x 2 :y 2 } "CA3" ]
+                                [ :text { :fill "white" :font-family "Arial" :font-size "22px" :x 0 :y 0 } "CA3" ] ] ]
+                        [ :g { :transform " translate(51,-159)" }
+                            [ :image { :height 159 :width 79 :x 0 :xlink:href "http://localhost/ti3/Ships/Yellow/Unit-Yellow-Dreadnaught.png" :y 0 } ]
+                            [ :g { :id "shaded-text" :transform " translate(0,119)" }
+                                [ :text { :fill "black" :font-family "Arial" :font-size "22px" :x 2 :y 2 } "DR7" ]
+                                [ :text { :fill "white" :font-family "Arial" :font-size "22px" :x 0 :y 0 } "DR7" ] ] ] ]
+                    [ :g { :id "abyz-fria-loc-label" :transform " translate(25,200)" }
+                        [ :text { :fill "black" :font-family "Arial" :font-size "36px" :x 2 :y 2 } "A1" ]
+                        [ :text { :fill "white" :font-family "Arial" :font-size "36px" :x 0 :y 0 } "A1" ] ] ]
+                [ :g { :id "aah-system" :transform " translate(324,188)" }
+                    [ :image { :height 376 :width 432 :x 0 :xlink:href "http://localhost/ti3/Tiles/1planet/Tile-Aah.gif" :y 0 } ]
+                    [ :g { :id "aah-units" :transform " translate(216,188)" }
+                        [ :g { :transform " translate(-115,62)" }
+                            [ :image { :height 36 :width 50 :x 0 :xlink:href "http://localhost/ti3/Ships/Red/Unit-Red-Fighter.png" :y 0 } ]
+                            [ :g { :id "shaded-text" :transform " translate(50,40)" }
+                                [ :text { :fill "black" :font-family "Arial" :font-size "45px" :x 2 :y 2 } "2" ]
+                                [ :text { :fill "white" :font-family "Arial" :font-size "45px" :x 0 :y 0 } "2" ] ] ] ]
+                    [ :g { :id "aah-loc-label" :transform " translate(25,200)" }
+                        [ :text { :fill "black" :font-family "Arial" :font-size "36px" :x 2 :y 2 } "B1" ]
+                        [ :text { :fill "white" :font-family "Arial" :font-size "36px" :x 0 :y 0 } "B1" ] ] ] ] ]
     )))
 
 (deftest find-planet-test
