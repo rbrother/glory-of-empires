7bae879d3b1c9e7773f414bf71df90d2d9d62e51
Robert Brotherus <robert.brotherus@napa.fi>
2016-01-06 04:36:55 +0200

    Added testing game-state with many ships

---
+++ many-ships-gamestate.clj
@@ -0,0 +1,89 @@
+{   :map {   :a3 {   :controller :norr
+            :id :a3
+            :logical-pos [ -2 0 ]
+            :planets { :perro { } :senno { } }
+            :ships {   :de11 { :id :de11 :owner :norr :type :de }
+                :dr1 { :id :dr1 :owner :norr :type :dr }
+                :dr2 { :id :dr2 :owner :norr :type :dr }
+                :dr3 { :id :dr3 :owner :norr :type :dr }
+                :dr4 { :id :dr4 :owner :norr :type :dr } }
+            :system :perro-senno }
+        :a4 { :id :a4 :logical-pos [ -2 1 ] :planets { } :system :asteroid-field }
+        :a5 { :id :a5 :logical-pos [ -2 2 ] :planets { :aah { } } :system :aah }
+        :b2 { :id :b2 :logical-pos [ -1 -1 ] :planets { } :system :ion-storm }
+        :b3 {   :controller :hacan
+            :id :b3
+            :logical-pos [ -1 0 ]
+            :planets { }
+            :ships {   :ca1 { :id :ca1 :owner :hacan :type :ca }
+                :ca10 { :id :ca10 :owner :hacan :type :ca }
+                :ca2 { :id :ca2 :owner :hacan :type :ca }
+                :ca3 { :id :ca3 :owner :hacan :type :ca }
+                :ca4 { :id :ca4 :owner :hacan :type :ca }
+                :ca5 { :id :ca5 :owner :hacan :type :ca }
+                :ca6 { :id :ca6 :owner :hacan :type :ca }
+                :ca7 { :id :ca7 :owner :hacan :type :ca }
+                :ca8 { :id :ca8 :owner :hacan :type :ca }
+                :ca9 { :id :ca9 :owner :hacan :type :ca } }
+            :system :empty }
+        :b4 {   :controller :norr
+            :id :b4
+            :logical-pos [ -1 1 ]
+            :planets { :rigel-i { } :rigel-ii { } :rigel-iii { } }
+            :ships {   :cr10 { :id :cr10 :owner :norr :type :cr }
+                :cr7 { :id :cr7 :owner :norr :type :cr }
+                :cr8 { :id :cr8 :owner :norr :type :cr }
+                :cr9 { :id :cr9 :owner :norr :type :cr } }
+            :system :rigel }
+        :b5 { :id :b5 :logical-pos [ -1 2 ] :planets { } :system :wormhole-b }
+        :c1 {   :controller :hacan
+            :id :c1
+            :logical-pos [ 0 -2 ]
+            :planets { :anhur { } }
+            :ships {   :cr1 { :id :cr1 :owner :hacan :type :cr }
+                :cr2 { :id :cr2 :owner :hacan :type :cr }
+                :cr3 { :id :cr3 :owner :hacan :type :cr }
+                :cr4 { :id :cr4 :owner :hacan :type :cr }
+                :cr5 { :id :cr5 :owner :hacan :type :cr }
+                :cr6 { :id :cr6 :owner :hacan :type :cr } }
+            :system :anhur }
+        :c2 { :id :c2 :logical-pos [ 0 -1 ] :planets { :solitude { } } :system :solitude }
+        :c3 { :controller :norr :id :c3 :logical-pos [ 0 0 ] :planets { } :ships { :ws1 { :id :ws1 :owner :norr :type :ws } :ws2 { :id :ws2 :owner :norr :type :ws } } :system :wormhole-a }
+        :c4 { :id :c4 :logical-pos [ 0 1 ] :planets { :kanite { } :loc { } } :system :kanite }
+        :c5 { :id :c5 :logical-pos [ 0 2 ] :planets { } :system :empty }
+        :d1 {   :controller :hacan
+            :id :d1
+            :logical-pos [ 1 -2 ]
+            :planets { :ogdoad { } :resheph { } }
+            :ships {   :de1 { :id :de1 :owner :hacan :type :de }
+                :de10 { :id :de10 :owner :hacan :type :de }
+                :de2 { :id :de2 :owner :hacan :type :de }
+                :de3 { :id :de3 :owner :hacan :type :de }
+                :de4 { :id :de4 :owner :hacan :type :de }
+                :de5 { :id :de5 :owner :hacan :type :de }
+                :de6 { :id :de6 :owner :hacan :type :de }
+                :de7 { :id :de7 :owner :hacan :type :de }
+                :de8 { :id :de8 :owner :hacan :type :de }
+                :de9 { :id :de9 :owner :hacan :type :de } }
+            :system :resheph-ogdoad }
+        :d2 { :id :d2 :logical-pos [ 1 -1 ] :planets { :asgard3 { } } :system :asgard3 }
+        :d3 {   :controller :norr
+            :id :d3
+            :logical-pos [ 1 0 ]
+            :planets { :abaddon { } :ashtroth { } :loki { } }
+            :ships {   :cr11 { :id :cr11 :owner :norr :type :cr }
+                :cr12 { :id :cr12 :owner :norr :type :cr }
+                :cr13 { :id :cr13 :owner :norr :type :cr }
+                :cr14 { :id :cr14 :owner :norr :type :cr }
+                :cr15 { :id :cr15 :owner :norr :type :cr }
+                :cr16 { :id :cr16 :owner :norr :type :cr }
+                :cr17 { :id :cr17 :owner :norr :type :cr }
+                :cr18 { :id :cr18 :owner :norr :type :cr }
+                :cr19 { :id :cr19 :owner :norr :type :cr } }
+            :system :ashtroth-loki-abaddon }
+        :d4 { :id :d4 :logical-pos [ 1 1 ] :planets { :vega-major { } :vega-minor { } } :system :vega }
+        :e1 { :id :e1 :logical-pos [ 2 -2 ] :planets { :custos { } :xhal { } } :system :custos-xhal }
+        :e2 { :id :e2 :logical-pos [ 2 -1 ] :planets { :perimeter { } } :system :perimeter }
+        :e3 { :id :e3 :logical-pos [ 2 0 ] :planets { :inaak { } :loc { } :tech { } } :system :inaak } }
+    :players { :hacan { :id :hacan } :norr { :id :norr } }
+    :ship-counters { :ca 10 :cr 19 :de 11 :dr 4 :ws 2 } }
