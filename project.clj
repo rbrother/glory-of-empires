1b5d81a081be42177238c8671a5cac08343f0afd
Robert Brotherus <robert@iki.fi>
2018-04-02 09:34:33 +0300

    Move to use clojure-common from clojars. Fixes Cursive dependencies and debugging

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject glory-of-empires "0.1.1-SNAPSHOT"
+(defproject glory-of-empires "0.1.2-SNAPSHOT"
   :description "Space strategy board game"
   :url "http://brotherus.net/fixme"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [  [org.clojure/clojure "1.9.0-alpha17"]
-			;		[org.clojure/clojure "1.7.0"]
                    [ring "1.6.3"]
-                 [hiccup "1.0.5"] ]
+                 [hiccup "1.0.5"]
+                 [org.clojars.rbrother/clojure-common "0.1.3"] ]
   :main ^:skip-aot glory-of-empires.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
